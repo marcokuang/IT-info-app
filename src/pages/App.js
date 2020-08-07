73180527244db6028f9c819547debc03c2f34c9a
Marco Kuang <marcojkuang@gmail.com>
2020-08-07 10:50:50 -0700

    intial setup

---
+++ src/pages/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginPage from "pages/LoginPage";
+
+class App extends React.Component {
+  render() {
+    return <LoginPage />;
+  }
+}
+
+export default App;
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "assets/styles/loginPage.less";
+
+export default class LoginPage extends Component {
+  render() {
+    return <div className="login-page">LoginPage</div>;
+  }
+}
